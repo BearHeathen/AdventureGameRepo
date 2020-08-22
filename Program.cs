8d32f94e588287ecaf386c79ad56bfd925e268b4
bearheathen <brandondstewart@gmail.com>
2020-08-22 11:30:11 -0500

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AdventureGame
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            string characterName;
+
+            Console.WriteLine("Ending's Dawn");
+            Console.WriteLine("Welcome to the beginning of the end...");
+            Console.WriteLine("\nWhat is your name?\n");
+            characterName = Console.ReadLine();
+            Console.WriteLine("\nWelcome, " + characterName + ".\n");
+
+        }
+    }
+}
